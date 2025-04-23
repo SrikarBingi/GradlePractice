f090f9a74c8aa2648c4580793b06f81da3f527fa
Srikar Bingi <srikar.bingi@gmail.com>
2025-04-23 17:22:39 +0530

    Todo list api

--- src/main/java/com/todo/todo/TodoController.java
+++ src/main/java/com/todo/todo/TodoController.java
@@ -2,13 +2,14 @@ package com.todo.todo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.springframework.web.bind.annotation.ResponseStatus;
 // import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -68,4 +69,19 @@ public class TodoController {
             }
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    
+        //Delete a todo
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable int id) {
+        Optional<Todo> optionalTodo = todoList.stream()
+                .filter(todo -> todo.getId() == id)
+                .findFirst();
+
+        if (optionalTodo.isPresent()) {
+            todoList.remove(optionalTodo.get());
+            return ResponseEntity.noContent().build(); // 204 No Content
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
