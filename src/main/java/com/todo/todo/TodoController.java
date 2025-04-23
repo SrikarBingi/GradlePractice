23578c4d77e0a4535310ac2be326fe5986f50393
Srikar Bingi <srikar.bingi@gmail.com>
2025-04-23 17:06:58 +0530

    Gradle first commit

---
+++ src/main/java/com/todo/todo/TodoController.java
@@ -0,0 +1,71 @@
+package com.todo.todo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final List<Todo> todoList = new ArrayList<>();
+
+     // GET all todos
+    @GetMapping("/")
+    public List<Todo> getAllTodos() {
+        return todoList;
+    }
+
+    // GET todo by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable int id) {
+        return todoList.stream()
+                .filter(todo -> todo.getId() == id)
+                .findFirst()
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // POST a new todo
+    @PostMapping("/")
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo newTodo) {
+        todoList.add(newTodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Todo> updateTodoPartially(
+        @PathVariable int id,
+        @RequestBody Map<String, Object> updates){
+
+            Optional<Todo> optionalTodo = todoList.stream()
+            .filter(todo -> todo.getId() == id)
+            .findFirst();
+
+            if (optionalTodo.isPresent()) {
+                Todo todo = optionalTodo.get();
+        
+                // Only update the 'completed' field if it is present in the request
+                if (updates.containsKey("completed")) {
+                    todo.setCompleted((Boolean) updates.get("completed"));
+                }
+
+                if(updates.containsKey("title")){
+                    todo.setTitle((String) updates.get("title"));
+                }
+        
+                return ResponseEntity.ok(todo);
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+}
